427b8a0c630cbbc9a65a9db771b36dd387c79d41
Jesus Gastelum <gastelumtec21@gmail.com>
2023-05-10 22:31:13 -0700

    coordinador page casi terminada

--- frontend/src/trasladatec/pages/CoordinadorPage.jsx
+++ frontend/src/trasladatec/pages/CoordinadorPage.jsx
@@ -1,4 +1,4 @@
-import { Divider, Grid, List, Typography } from "@mui/material"
+import { Button, Divider, Grid, List, ListItem, ListItemText, Typography } from "@mui/material"
 import { TrasladaTECLayout } from "../layout/TrasladaTECLayout"
 
 export const CoordinadorPage = () => {
@@ -14,7 +14,7 @@ export const CoordinadorPage = () => {
         >
             <Grid container
                 className="box-shadow"
-                direction="row"
+                direction="column"
                 sx={{
                     backgroundColor: 'secondary.main',
                     width: '80%',
@@ -23,18 +23,76 @@ export const CoordinadorPage = () => {
                     padding: 2
                 }}
             >      
-                <Grid item
-                    xs={12}
-                    sx={{
-                        width: '100%'
-                    }}
-                >
-                    <Typography align='center' variant="h4" gutterBottom>Traslados</Typography>
-                    <Divider />
+                <Grid item>
+                    <Typography 
+                        align='center' 
+                        variant="h4" 
+                        sx={{
+                            borderBottom: '2px solid black',
+                            padding: 2,
+                            mb: 3
+                        }}
+                    >
+                        Traslados
+                    </Typography>
                 </Grid>
-                <List>
+                <Grid item>
+                    
+                    <List sx={{ width: '100%'}}>
+                        <ListItem
+                            className="box-shadow" 
+                            sx={{ 
+                                width: '100%', 
+                                backgroundColor: 'secondary.light', 
+                                borderRadius: 3,
+                                padding: 2,
+                                mb: 2
+                            }}
+                        >
+                            <Grid container
+                                direction='row'
+                                justifyContent='space-between'
+                                sx={{
+                                    width: '100%'
+                                }}
+                            >
+                                <Grid item
+                                
+                                >
+                                    <ListItemText>Folio: 1234</ListItemText>
+                                    <ListItemText>Fecha: 10/05/2023</ListItemText>
+                                    <ListItemText>Instituto destino: Tecnologico de Navolato </ListItemText>
+                                    <ListItemText>Estado: Pendiente </ListItemText>
+                                </Grid>
+                                
+                                <Grid item>
+                                    <Grid container
+                                        direction='column'
+                                        spacing={2}
+                                        alignItems='center'
+                                    >
+                                        <Grid item>
+                                            <Button variant='contained'>Ver detalle</Button>
+                                        </Grid>
+                                        <Grid item>
+                                            <Button 
+                                                variant="outlined" 
+                                                color="error"
+                                                sx={{
+                                                    mr: 2
+                                                }}
+                                            >
+                                                Rechazar
+                                            </Button>
+                                            <Button variant="outlined" color="success">Aceptar</Button>
+                                        </Grid>
+                                    </Grid>
+                                </Grid>   
+                            </Grid>
+                        </ListItem>
+                    </List>
+                </Grid>
 
-                </List>
             </Grid>
         </Grid>
     </TrasladaTECLayout>
